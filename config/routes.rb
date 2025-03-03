c0f5bd8a74bb94a52a1f440b7ca262acff1d03b4
dalalnotes . <dalal.aldilaimi@kellogg.northwestern.edu>
2025-03-03 03:19:16 +0000

    hw3dalal

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+Rails.application.routes.draw do
+  root "places#index"
+  
+  resources :places, only: [:index, :show, :new, :create] do
+    resources :entries, only: [:new, :create]
+  end
 end
